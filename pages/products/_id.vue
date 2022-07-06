3c535983d6ab5f833b510bb1712e18c6bacaf041
takumi-mat <takumi.tabippo@gmail.com>
2022-07-06 19:06:52 +0900

    modal

--- pages/products/_id.vue
+++ pages/products/_id.vue
@@ -656,8 +656,10 @@ export default {
 			position: absolute;
 			top: 0;
 			left: 0;
+			right: 0;
+			bottom: 0;
 			width: 100vw;
-			height: 100vh;
+			// height: 100vh;
 			opacity: 0;
 			z-index: -1;
 			background-color: rgba(0, 0, 0, 0.5);
